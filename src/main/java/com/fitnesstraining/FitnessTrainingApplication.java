4252f006765d6139aab921fd08e8fed0b1338e18
Jehaierys <kawaysempai@gmail.com>
2026-06-28 00:42:11 +0500

    Idea Initialization

---
+++ src/main/java/com/fitnesstraining/FitnessTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.fitnesstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessTrainingApplication.class, args);
+    }
+
+}
