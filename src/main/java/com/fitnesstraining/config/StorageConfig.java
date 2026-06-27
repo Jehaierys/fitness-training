bc7c3dd3075e2d012cc9ac243ccccbfa91fabc91
Jehaierys <kawaysempai@gmail.com>
2026-06-28 01:41:39 +0500

    feat: StorageConfig class with data source beans created

---
+++ src/main/java/com/fitnesstraining/config/StorageConfig.java
@@ -0,0 +1,31 @@
+package com.fitnesstraining.config;
+
+
+import com.fitnesstraining.domain.Coach;
+import com.fitnesstraining.domain.Session;
+import com.fitnesstraining.domain.Trainee;
+import com.fitnesstraining.domain.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Configuration
+public class StorageConfig {
+    @Bean
+    public Map<Long, User> userStorage() {
+        return new ConcurrentHashMap<>();
+    }
+    @Bean
+    public Map<Long, Coach> mentorStorage() {
+        return new ConcurrentHashMap<>();
+    }
+    @Bean
+    public Map<Long, Trainee> traineeStorage() {
+        return new ConcurrentHashMap<>();
+    }
+    @Bean
+    public Map<Long, Session> sessionStorage() {
+        return new ConcurrentHashMap<>();
+    }
+}
