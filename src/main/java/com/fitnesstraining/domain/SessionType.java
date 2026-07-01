3d8fc5da568679779f57727d5562bc6043fd71e8
Jehaierys <kawaysempai@gmail.com>
2026-07-01 17:04:44 +0500

    feat: SessionType becomes an @Entity

--- src/main/java/com/fitnesstraining/domain/SessionType.java
+++ src/main/java/com/fitnesstraining/domain/SessionType.java
@@ -1,4 +1,37 @@
 package com.fitnesstraining.domain;
 
-public enum SessionType {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "session_types")
+public class SessionType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "session_type_seq")
+    @SequenceGenerator(name = "session_type_seq", sequenceName = "session_type_id_seq", allocationSize = 1)
+    private Long id;
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "coach_session_type",
+            joinColumns = @JoinColumn(name = "session_type_id"),
+            inverseJoinColumns = @JoinColumn(name = "coach_id")
+    )
+    private Set<Coach> coaches = new HashSet<>();
+
+   @OneToMany(
+           mappedBy = "sessionType",
+           cascade = CascadeType.ALL,
+           orphanRemoval = true
+   )
+    private Set<Session> sessions;
 }
