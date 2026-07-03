8ece45d8ae1782a9fd557a1535da0768e72265cf
Jehaierys <kawaysempai@gmail.com>
2026-07-04 04:26:02 +0500

    fix & refactor: now service classes suit new repositories. Some ecxeption throwing code was shorten by lambda

--- src/main/java/com/fitnesstraining/service/DefaultUserService.java
+++ src/main/java/com/fitnesstraining/service/DefaultUserService.java
@@ -4,11 +4,12 @@ package com.fitnesstraining.service;
 import com.fitnesstraining.domain.User;
 import com.fitnesstraining.repository.UserRepository;
 import com.fitnesstraining.service.abstraction.UserService;
-import com.fitnesstraining.service.exception.NotFoundException;
 import com.fitnesstraining.service.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import static com.fitnesstraining.utils.ExceptionSuppliers.UserNotFound;
 
 
 @Slf4j
@@ -19,51 +20,39 @@ public class DefaultUserService implements UserService {
     private final UserRepository userRepository;
 
     public User create(User user) {
-        log.info("Creating user with username: {}", user.getUsername());
-        return userRepository.save(user);
+        return userRepository.create(user);
     }
 
     public User getById(Long id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("User not found with id: " + id));
+                .orElseThrow(UserNotFound("User not found with id: " + id));
     }
 
     public User update(User user) {
-        if (userRepository.existsById(user.getId())) {
-            log.info("Updating user with id: {}", user.getId());
-            return userRepository.save(user);
-        } else {
-            log.warn("User not found with id: {} to be updated", user.getId());
-            throw new UserNotFoundException();
-        }
+        return userRepository.update(user);
     }
 
     public void delete(Long id) {
-        if (!userRepository.existsById(id)) {
-            log.warn("User not found with id: {} to be deleted", id);
-            throw new UserNotFoundException("User not found with id: " + id);
-        }
-        log.info("Deleting user with id: {}", id);
         userRepository.deleteById(id);
     }
 
     public boolean existsByUsername(String username) {
-        return userRepository.existsByUsername(username);
+        return userRepository.existByUsername(username);
     }
 
     public void setActive(Long id, boolean isActive) {
         userRepository.findById(id).ifPresentOrElse(user -> {
             user.setActive(isActive);
-            userRepository.save(user);
+            userRepository.update(user);
         }, () -> {
-            throw new NotFoundException("User not found with id: " + id);
+            throw new UserNotFoundException("User not found with id: " + id);
         });
         log.info("Set active status for user with id: {} to {}", id, isActive);
     }
 
     public void checkCredentials(String username, String password) {
-        User user = userRepository.findUserByUsername(username)
-                .orElseThrow(() -> new NotFoundException("User not found with username: " + username));
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(UserNotFound("User not found with username: " + username));
 
         if (!user.getPassword().equals(password)) {
             log.warn("User not found with username: {} and password: {}", username, user.getPassword());
@@ -76,9 +65,9 @@ public class DefaultUserService implements UserService {
     // we can authenticate this request with UsernamePasswordAuthentication filter
     // then check for ids matching by jwt
     public void newPassword(Long id, String newPassword) {
-        User user = userRepository.findById(id).orElseThrow(() -> new NotFoundException("User not found with id: " + id));
+        User user = userRepository.findById(id).orElseThrow(UserNotFound("\"User not found with id: \" + id"));
         user.setPassword(newPassword);
-        userRepository.save(user);
+        userRepository.update(user);
         log.info("Password updated for user with id: {}", id);
     }
 }
