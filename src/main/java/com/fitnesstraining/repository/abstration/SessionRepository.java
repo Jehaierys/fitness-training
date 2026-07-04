93231c0bccb7adbfa5944f3dfdec469bcfbaab35
Jehaierys <kawaysempai@gmail.com>
2026-07-04 19:26:39 +0500

    feat: repository abstractions created

---
+++ src/main/java/com/fitnesstraining/repository/abstration/SessionRepository.java
@@ -0,0 +1,17 @@
+package com.fitnesstraining.repository.abstration;
+
+import com.fitnesstraining.domain.Session;
+import com.fitnesstraining.dto.SessionSearchCriteria;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SessionRepository {
+
+    Session create(Session session);
+
+    Optional<Session> findById(Long id);
+
+    List<Session> searchSessions(SessionSearchCriteria criteria);
+
+}
