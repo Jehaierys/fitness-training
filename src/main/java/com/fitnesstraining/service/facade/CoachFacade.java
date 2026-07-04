9c7fe5248d79b4a96fee99d591eb504cb3384c1a
Jehaierys <kawaysempai@gmail.com>
2026-07-04 05:37:18 +0500

    feat: profile updating functionality implemented, new dto created.

--- src/main/java/com/fitnesstraining/service/facade/CoachFacade.java
+++ src/main/java/com/fitnesstraining/service/facade/CoachFacade.java
@@ -2,6 +2,7 @@ package com.fitnesstraining.service.facade;
 
 import com.fitnesstraining.domain.Coach;
 import com.fitnesstraining.domain.User;
+import com.fitnesstraining.dto.CoachProfileUpdateRequest;
 import com.fitnesstraining.dto.CoachSignUpRequest;
 import com.fitnesstraining.service.abstraction.CoachService;
 import com.fitnesstraining.service.abstraction.UserService;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
+
 
 
 @Slf4j
@@ -52,5 +54,23 @@ public class CoachFacade {
         log.info("Successfully created coach: {} {}, specialization: {}, userId: {} process's UUID: {}", request.getFirstName(), request.getLastName(), request.getSpecialization(), user.getId(), uuid);
 
         return coachService.create(coach);
+    }
+
+    public Coach updateProfile(CoachProfileUpdateRequest request) {
+        UUID uuid = UUID.randomUUID();
+        log.info("Updating coach profile for id: {}, process's UUID: {}", request.getId(), uuid);
+
+        Coach existingCoach = coachService.getById(request.getId());
+        User existingUser = existingCoach.getUser();
+
+        existingUser.setFirstName(request.getFirstName());
+        existingUser.setLastName(request.getLastName());
+        userService.update(existingUser);
+
+        existingCoach.setSpecialization(request.getSpecialization());
+        coachService.update(existingCoach);
+
+        log.info("Successfully updated coach profile for id: {}, process's UUID: {}", request.getId(), uuid);
+        return existingCoach;
     }
 }
--- src/main/java/com/fitnesstraining/service/facade/TraineeFacade.java
+++ src/main/java/com/fitnesstraining/service/facade/TraineeFacade.java
@@ -2,6 +2,7 @@ package com.fitnesstraining.service.facade;
 
 import com.fitnesstraining.domain.Trainee;
 import com.fitnesstraining.domain.User;
+import com.fitnesstraining.dto.TraineeProfileUpdateRequest;
 import com.fitnesstraining.dto.TraineeSignUpRequest;
 import com.fitnesstraining.service.abstraction.TraineeService;
 import com.fitnesstraining.service.abstraction.UserService;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
+
 
 
 @Slf4j
@@ -53,5 +55,24 @@ public class TraineeFacade {
         log.info("Successfully created trainee: {} {}, birth date: {}, address: {}, userId: {} process's UUID: {}", request.getFirstName(), request.getLastName(), request.getBirthDate(), request.getAddress(), user.getId(), uuid);
 
         return traineeService.create(trainee);
+    }
+
+    public Trainee updateProfile(TraineeProfileUpdateRequest request) {
+        UUID uuid = UUID.randomUUID();
+        log.info("Updating trainee profile for id: {}, process's UUID: {}", request.getId(), uuid);
+
+        Trainee existingTrainee = traineeService.getById(request.getId());
+        User existingUser = existingTrainee.getUser();
+
+        existingUser.setFirstName(request.getFirstName());
+        existingUser.setLastName(request.getLastName());
+        userService.update(existingUser);
+
+        existingTrainee.setBirthDate(request.getBirthDate());
+        existingTrainee.setAddress(request.getAddress());
+        traineeService.update(existingTrainee);
+
+        log.info("Successfully updated trainee profile for id: {}, process's UUID: {}", request.getId(), uuid);
+        return existingTrainee;
     }
 }
