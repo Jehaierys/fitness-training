f5c841821923cbc789d2cbc047432f8fe83c0170
Jehaierys <kawaysempai@gmail.com>
2026-07-10 06:16:59 +0500

    feat: new methods in controllers

--- src/main/java/com/fitnesstraining/api/CoachController.java
+++ src/main/java/com/fitnesstraining/api/CoachController.java
@@ -1,15 +1,16 @@
 package com.fitnesstraining.api;
 
+import com.fitnesstraining.domain.dto.abstraction.UpdateUserProfileRequest;
 import com.fitnesstraining.domain.dto.coach.request.CoachSignUpRequest;
 import com.fitnesstraining.domain.dto.abstraction.UserSignUpResponse;
 import com.fitnesstraining.api.openapi.CoachControllerApi;
+import com.fitnesstraining.domain.dto.coach.response.UpdateCoachProfileResponse;
 import com.fitnesstraining.logic.facade.CoachFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @Slf4j
 @RestController
@@ -21,10 +22,15 @@ public class CoachController implements CoachControllerApi {
 
     public ResponseEntity<UserSignUpResponse> signUp(CoachSignUpRequest request) {
         log.info("Received signup request for coach: {}", request.getFirstName());
-
         return new ResponseEntity<>(
                 coachFacade.signUp(request),
                 HttpStatus.CREATED
         );
     }
+
+    public ResponseEntity<UpdateCoachProfileResponse> updateProfile(UpdateUserProfileRequest request) {
+        return ResponseEntity.ok(coachFacade.updateProfile(request));
+    }
 }
+
+
--- src/main/java/com/fitnesstraining/api/TraineeController.java
+++ src/main/java/com/fitnesstraining/api/TraineeController.java
@@ -1,8 +1,11 @@
 package com.fitnesstraining.api;
 
+import com.fitnesstraining.domain.dto.abstraction.UpdateUserProfileRequest;
 import com.fitnesstraining.domain.dto.trainee.request.TraineeSignUpRequest;
 import com.fitnesstraining.domain.dto.abstraction.UserSignUpResponse;
 import com.fitnesstraining.api.openapi.TraineeControllerApi;
+import com.fitnesstraining.domain.dto.trainee.request.UpdateTraineeProfileRequest;
+import com.fitnesstraining.domain.dto.trainee.response.UpdateTraineeProfileResponse;
 import com.fitnesstraining.logic.facade.TraineeFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +22,15 @@ public class TraineeController implements TraineeControllerApi {
 
 
     public ResponseEntity<UserSignUpResponse> signUp(TraineeSignUpRequest request) {
-        log.info("Received signup request for trainee: {}", request.getFirstName());
-
+        log.info("Received signup request for trainee: {} {}", request.getFirstName(), request.getLastName());
         return new ResponseEntity<>(
                 traineeFacade.signUp(request),
                 HttpStatus.CREATED
         );
+    }
+
+    public ResponseEntity<UpdateTraineeProfileResponse> updateProfile(UpdateTraineeProfileRequest request) {
+        log.info("Received update profile request for trainee: {}", request.getUsername());
+        return ResponseEntity.ok(traineeFacade.updateProfile(request));
     }
 }
