6017eca1c9d40947c6aae7bdf00536b213489270
Jehaierys <kawaysempai@gmail.com>
2026-07-14 13:16:45 +0500

    feat: SecurityConfig file modified. Form Login was upgraded: it gets ui stubs, some parameters were specified manually.

--- src/main/java/com/fitnesstraining/config/security/SecurityConfig.java
+++ src/main/java/com/fitnesstraining/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.fitnesstraining.config.security;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,15 +10,22 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    private final GrantedAuthority roleCoach = new SimpleGrantedAuthority("ROLE_COACH");
+    private final GrantedAuthority roleTrainee = new SimpleGrantedAuthority("ROLE_TRAINEE");
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,7 +43,25 @@ public class SecurityConfig {
                 )
 
                 // UsernamePasswordAuthenticationFilter enabled on POST /login:
-                .formLogin(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/authentication.html")
+                        .loginProcessingUrl("/login")
+                        .permitAll()
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .successHandler((request, response, authentication) -> {
+
+                            Collection<? extends GrantedAuthority> roles = authentication.getAuthorities();
+
+                            if (roles.contains(roleTrainee)) {
+                                response.sendRedirect("/trainee.html");
+                            } else if (roles.contains(roleCoach)) {
+                                response.sendRedirect("/coach.html");
+                            } else {
+                                response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                            }
+                        })
+                )
 
                 .httpBasic(AbstractHttpConfigurer::disable)
 
