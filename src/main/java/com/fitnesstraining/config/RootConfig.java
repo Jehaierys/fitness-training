993030724091fb9d6d727575ee1d50407abeef2a
Jehaierys <kawaysempai@gmail.com>
2026-07-15 14:24:39 +0500

    clean: remove .properties due to @Bean configuration

--- src/main/java/com/fitnesstraining/config/RootConfig.java
+++ src/main/java/com/fitnesstraining/config/RootConfig.java
@@ -3,18 +3,11 @@ package com.fitnesstraining.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
-@Profile({"prod", "dev"})
 @ComponentScan("com.example")
-@PropertySource(value = {
-        "classpath:application.properties",
-        "classpath:application-dev.properties"
-})
 public class RootConfig {
 
 }
