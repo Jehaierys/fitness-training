2dc2600c940312cf02c80ba4a6bf3bbf81839c0b
Jehaierys <kawaysempai@gmail.com>
2026-07-15 14:19:29 +0500

    feat: new Main class

--- src/main/java/com/fitnesstraining/Application.java
+++ src/main/java/com/fitnesstraining/Application.java
@@ -1,13 +1,48 @@
 package com.fitnesstraining;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.fitnesstraining.config.DataConfig;
+import com.fitnesstraining.config.RootConfig;
+import com.fitnesstraining.config.web.WebMvcConfig;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-@SpringBootApplication
-public class FitnessTrainingApplication {
+import java.io.File;
 
-    public static void main(String[] args) {
-        SpringApplication.run(FitnessTrainingApplication.class, args);
+
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        StandardContext context = (StandardContext) tomcat.addContext("", new File(".").getAbsolutePath());
+
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+
+        applicationContext.register(
+                RootConfig.class,
+                WebMvcConfig.class,
+                DataConfig.class
+        );
+
+        DispatcherServlet dispatcherServlet =
+                new DispatcherServlet(applicationContext);
+
+        Tomcat.addServlet(context, "dispatcher", dispatcherServlet);
+
+        context.addServletMappingDecoded("/", "dispatcher");
+
+        tomcat.start();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("Shalom");
+        System.out.println();
+
+        tomcat.getServer().await();
     }
-
 }
