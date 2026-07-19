a62d208f14a976e17d925d8eb1baa82b48d1f6eb
Jehaierys <kawaysempai@gmail.com>
2026-07-19 16:02:16 +0500

    test: add short Specialization creation

---
+++ src/test/java/com/fitnesstraining/testUtils/Specializations.java
@@ -0,0 +1,56 @@
+package com.fitnesstraining.testUtils;
+
+
+import com.fitnesstraining.domain.entity.SessionType;
+import com.fitnesstraining.testUtils.entity.SessionTypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Specializations {
+
+    private Specializations() {}
+
+    public static List<SessionType> yogaPilates() {
+        return new ArrayList<>(
+                List.of(
+                        SessionTypes.yoga(),
+                        SessionTypes.pilates()
+                )
+        );
+    }
+
+    public static List<SessionType> pilates() {
+        return new ArrayList<>(
+                List.of(SessionTypes.pilates())
+        );
+    }
+
+    public static List<SessionType> cardioStrengthTraining() {
+        return new ArrayList<>(
+                List.of(
+                        SessionTypes.cardio(),
+                        SessionTypes.strengthTraining()
+                )
+        );
+    }
+
+    public static List<SessionType> allSpecializations() {
+        return new ArrayList<>(
+                List.of(
+                        SessionTypes.yoga(),
+                        SessionTypes.crossfit(),
+                        SessionTypes.pilates(),
+                        SessionTypes.cardio(),
+                        SessionTypes.strengthTraining(),
+                        SessionTypes.zumba()
+                )
+        );
+    }
+
+    public static List<SessionType> cardio() {
+        return new ArrayList<>(
+                List.of(SessionTypes.cardio())
+        );
+    }
+}
