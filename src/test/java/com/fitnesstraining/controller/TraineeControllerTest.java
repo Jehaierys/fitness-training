8ff691d71fc582dcffddd0c4f0844f570f7b74e5
Jehaierys <kawaysempai@gmail.com>
2026-07-21 00:31:02 +0500

    test: Trainee Controller Test class implemented

--- src/test/java/com/fitnesstraining/controller/TraineeControllerTest.java
+++ src/test/java/com/fitnesstraining/controller/TraineeControllerTest.java
@@ -1,5 +1,6 @@
 package com.fitnesstraining.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fitnesstraining.api.TraineeController;
 import com.fitnesstraining.config.JacksonTestConfig;
 import com.fitnesstraining.domain.dto.abstraction.Activated;
@@ -23,18 +24,19 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.test.web.servlet.RequestBuilder;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
 // Checks proper validation
 // And correct response status
+
+// Register, Update operations are covered
 
 // CHECKSTYLE.OFF
 @WebMvcTest(TraineeController.class)
@@ -132,8 +134,19 @@ public class TraineeControllerTest {
 
         // should be moved to the upcoming UserController
         @Test
-        void setAvailable() throws Exception {
+        void setAvailable_Activate() throws Exception {
             activated = Activated.builder().isActive(true).username("sophia.miller").build();
+
+            mockMvc.perform(patch("/v1-0-0/trainees")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(activated)))
+                    .andExpect(status().isOk());
+        }
+
+        // should be moved to the upcoming UserController
+        @Test
+        void setAvailable_Deactivate() throws Exception {
+            activated = Activated.builder().isActive(false).username("sophia.miller").build();
 
             mockMvc.perform(patch("/v1-0-0/trainees")
                             .contentType(MediaType.APPLICATION_JSON)
@@ -144,15 +157,19 @@ public class TraineeControllerTest {
 
 
     @Nested
-    class InvalidInput_ShouldNotThrowException {
+    class InvalidRegistrationRequest_ExceptionsExpected {
+
+        private RequestBuilder template(RegisterTraineeRequest request) throws JsonProcessingException {
+            return post("/v1-0-0/trainees")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request));
+        }
 
         @Test
-        void testing() throws Exception {
+        void cannotBeBlank() throws Exception {
             registerRequest = RegisterTraineeRequests.Invalid.missingCrucialData();
 
-            mockMvc.perform(post("/v1-0-0/trainees")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(registerRequest)))
+            mockMvc.perform(template(registerRequest))
                     .andExpect(status().isBadRequest())
                     .andExpect(jsonPath("$.details.firstName")
                             .value(ValidationErrorMessages.FirstName.CANNOT_BE_BLANK))
@@ -162,6 +179,146 @@ public class TraineeControllerTest {
                             .value(ValidationErrorMessages.Username.CANNOT_BE_BLANK))
                     .andExpect(jsonPath("$.details.password")
                             .value(ValidationErrorMessages.Password.CANNOT_BE_BLANK));
+        }
+
+        // Idea generated this entire test on its own with 1 attempt
+        @Test
+        void tooShort() throws Exception {
+            registerRequest = RegisterTraineeRequests.Invalid.tooShort();
+
+            mockMvc.perform(template(registerRequest))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.details.username")
+                            .value(ValidationErrorMessages.Username.SIZE))
+                    .andExpect(jsonPath("$.details.firstName")
+                            .value(ValidationErrorMessages.FirstName.SIZE))
+                    .andExpect(jsonPath("$.details.lastName")
+                            .value(ValidationErrorMessages.LastName.SIZE))
+                    .andExpect(jsonPath("$.details.password")
+                            .value(ValidationErrorMessages.Password.SIZE))
+                    .andExpect(jsonPath("$.details.address")
+                            .value(ValidationErrorMessages.Address.SIZE));
+        }
+
+        @Test
+        void tooLong() throws Exception {
+            registerRequest = RegisterTraineeRequests.Invalid.tooLong();
+
+            mockMvc.perform(template(registerRequest))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.details.username")
+                            .value(ValidationErrorMessages.Username.SIZE))
+                    .andExpect(jsonPath("$.details.firstName")
+                            .value(ValidationErrorMessages.FirstName.SIZE))
+                    .andExpect(jsonPath("$.details.lastName")
+                            .value(ValidationErrorMessages.LastName.SIZE))
+                    .andExpect(jsonPath("$.details.password")
+                            .value(ValidationErrorMessages.Password.SIZE))
+                    .andExpect(jsonPath("$.details.address")
+                            .value(ValidationErrorMessages.Address.SIZE));
+        }
+
+        @Test
+        void forbiddenCharacter() throws Exception {
+            registerRequest = RegisterTraineeRequests.Invalid.forbiddenCharacter();
+
+            mockMvc.perform(template(registerRequest))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.details.username")
+                            .value(ValidationErrorMessages.Username.PATTERN))
+                    .andExpect(jsonPath("$.details.firstName")
+                            .value(ValidationErrorMessages.FirstName.PATTERN))
+                    .andExpect(jsonPath("$.details.lastName")
+                            .value(ValidationErrorMessages.LastName.PATTERN));
+        }
+
+        @Test
+        void invalidDate() throws Exception {
+            registerRequest = RegisterTraineeRequests.Invalid.invalidDate();
+
+            mockMvc.perform(template(registerRequest))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.details.birthDate")
+                            .value(ValidationErrorMessages.BirthDate.PAST));
+        }
+    }
+
+    @Nested
+    class InvalidUpdateRequest_ExceptionsExpected {
+
+        private RequestBuilder template(UpdateTraineeRequest request) throws JsonProcessingException {
+            return put("/v1-0-0/trainees")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request));
+        }
+
+        @Test
+        void cannotBeBlank() throws Exception {
+            updateRequest = UpdateTraineeRequests.Invalid.missingCrucialData();
+
+            mockMvc.perform(template(updateRequest))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.details.firstName")
+                            .value(ValidationErrorMessages.FirstName.CANNOT_BE_BLANK))
+                    .andExpect(jsonPath("$.details.lastName")
+                            .value(ValidationErrorMessages.LastName.CANNOT_BE_BLANK))
+                    .andExpect(jsonPath("$.details.username")
+                            .value(ValidationErrorMessages.Username.CANNOT_BE_BLANK));
+        }
+
+        @Test
+        void tooShort() throws Exception {
+            updateRequest = UpdateTraineeRequests.Invalid.tooShort();
+
+            mockMvc.perform(template(updateRequest))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.details.username")
+                            .value(ValidationErrorMessages.Username.SIZE))
+                    .andExpect(jsonPath("$.details.firstName")
+                            .value(ValidationErrorMessages.FirstName.SIZE))
+                    .andExpect(jsonPath("$.details.lastName")
+                            .value(ValidationErrorMessages.LastName.SIZE));
+        }
+
+        @Test
+        void tooLong() throws Exception {
+            updateRequest = UpdateTraineeRequests.Invalid.tooLong();
+
+            mockMvc.perform(template(updateRequest))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.details.username")
+                            .value(ValidationErrorMessages.Username.SIZE))
+                    .andExpect(jsonPath("$.details.firstName")
+                            .value(ValidationErrorMessages.FirstName.SIZE))
+                    .andExpect(jsonPath("$.details.lastName")
+                            .value(ValidationErrorMessages.LastName.SIZE))
+                    .andExpect(jsonPath("$.details.address")
+                            .value(ValidationErrorMessages.Address.SIZE));
+        }
+
+        @Test
+        void forbiddenCharacter() throws Exception {
+            updateRequest = UpdateTraineeRequests.Invalid.forbiddenCharacter();
+
+            mockMvc.perform(template(updateRequest))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.details.username")
+                            .value(ValidationErrorMessages.Username.PATTERN))
+                    .andExpect(jsonPath("$.details.firstName")
+                            .value(ValidationErrorMessages.FirstName.PATTERN))
+                    .andExpect(jsonPath("$.details.lastName")
+                            .value(ValidationErrorMessages.LastName.PATTERN));
+        }
+
+
+        @Test
+        void futureBirthdate() throws Exception {
+            updateRequest = UpdateTraineeRequests.Invalid.futureBirthdate();
+
+            mockMvc.perform(template(updateRequest))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.details.birthDate")
+                            .value(ValidationErrorMessages.BirthDate.PAST));
         }
     }
 }
