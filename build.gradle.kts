e501fc823b2236bbed02b708c5ff6aff3c7864a8
Jehaierys <kawaysempai@gmail.com>
2026-07-26 05:59:54 +0500

    refactor: dependency section rearranged

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,32 +28,42 @@ extra["springCloudVersion"] = "2025.1.2"
 
 dependencies {
 
-
+    // Starters & Other
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//    implementation("org.springframework.boot:spring-boot-starter-flyway")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-//    implementation("org.flywaydb:flyway-database-postgresql")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
-//    implementation("org.springframework.cloud:spring-cloud-starter-vault-config")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-//    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     runtimeOnly("org.postgresql:postgresql")
+
+
+
+    // Later
+//    implementation("org.springframework.boot:spring-boot-starter-flyway")
+//    implementation("org.flywaydb:flyway-database-postgresql")
+//    implementation("org.springframework.cloud:spring-cloud-starter-vault-config")
+//    testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
+//    testImplementation("org.springframework.security:spring-security-test")
+//    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+
+
+    // Test
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.testcontainers:testcontainers-junit-jupiter")
     testImplementation("org.testcontainers:testcontainers-postgresql")
-    annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
-//    testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
-//    testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 
 
     // Lombok & MapStruct
