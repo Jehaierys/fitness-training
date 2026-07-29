8bf820beef7101f0e6c028eb39142681ea900314
Jehaierys <kawaysempai@gmail.com>
2026-07-29 06:02:06 +0500

    test: Redis connection

---
+++ src/test/java/com/fitnesstraining/config/RedisTest.java
@@ -0,0 +1,20 @@
+package com.fitnesstraining.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisTest {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public void test() {
+        redisTemplate.opsForValue().set("test", "hello");
+
+        String value = redisTemplate.opsForValue().get("test");
+
+        System.out.println(value);
+    }
+}
