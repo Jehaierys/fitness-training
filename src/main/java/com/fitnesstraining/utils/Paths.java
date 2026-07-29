d3bd15b51aaec2289dbaa6b4ac3135a0fab2459f
Jehaierys <kawaysempai@gmail.com>
2026-07-29 06:02:48 +0500

    feat: start to keep urls as constants

---
+++ src/main/java/com/fitnesstraining/utils/Paths.java
@@ -0,0 +1,12 @@
+package com.fitnesstraining.utils;
+
+public final class Paths {
+
+    public static final String LOGIN_URL = "/login";
+
+    public static final String AUTHENTICATION_PAGE_URL = "/authentication.html";
+
+    public  static final String REGISTRATION_PAGE_URL = "/registration.html";
+
+    private Paths() {}
+}
