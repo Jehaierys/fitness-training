1a65499f96d32b18e7b63337e888f78d56ea00de
Jehaierys <kawaysempai@gmail.com>
2026-08-03 13:14:06 +0500

    fix

--- src/main/java/com/fitnesstraining/domain/dto/request/UpdateTraineeCoachesRequest.java
+++ src/main/java/com/fitnesstraining/domain/dto/request/UpdateTraineeCoachesRequest.java
@@ -1,13 +1,19 @@
 package com.fitnesstraining.domain.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Null;
 import lombok.Builder;
 import lombok.Value;
 
-import java.util.Set;
+import java.util.Collection;
 
 @Value
 @Builder
 public class UpdateTraineeCoachesRequest {
+
+    @Null(message = "traineeId must be null")
     Long traineeId;
-    Set<Long> coachIds;
+
+    @NotEmpty(message = "Coach IDs cannot be empty")
+    Collection<Long> coachIds;
 }
