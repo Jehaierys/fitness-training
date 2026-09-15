73d41c948c097cf300a5b1416328889665c3ade3
Jehaierys <kawaysempai@gmail.com>
2026-09-16 02:46:06 +0500

    test: Redis container refactored

--- src/test/java/com/fitnesstraining/config/test/RedisTestContainer.java
+++ src/test/java/com/fitnesstraining/config/test/RedisTestContainer.java
@@ -5,10 +5,8 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 import com.redis.testcontainers.RedisContainer;
 
-
 public interface RedisTestContainer {
 
     @Container
     RedisContainer REDIS = new RedisContainer(DockerImageName.parse("redis:7-alpine"));
-
 }
