8740c62a730c1a6107435732e346760cfe9cde1e
Jehaierys <kawaysempai@gmail.com>
2026-09-18 10:42:20 +0500

    fix: Criteris search

--- src/main/java/com/fitnesstraining/service/utils/CoachSearcher.java
+++ src/main/java/com/fitnesstraining/service/utils/CoachSearcher.java
@@ -2,22 +2,26 @@ package com.fitnesstraining.service.utils;
 
 import com.fitnesstraining.domain.dto.response.coach.CoachDto;
 import com.fitnesstraining.domain.entity.Coach;
+import com.fitnesstraining.domain.entity.Trainee;
 import com.fitnesstraining.service.mapper.CoachMapper;
 import com.fitnesstraining.repository.dsl.Criteria;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class CoachSearcher {
 
+    @PersistenceContext
+    private EntityManager entityManager;
     private final CoachMapper mapper;
-
 
     public List<CoachDto> findAvailable(Long traineeId) {
         final List<Coach> availableCoaches = fetchAvailableCoaches(traineeId);
@@ -25,10 +29,12 @@ public class CoachSearcher {
     }
 
     private List<Coach> fetchAvailableCoaches(Long traineeId) {
-        return Criteria.<Coach>of()
+        final Trainee traineeRef = entityManager.getReference(Trainee.class, traineeId);
+
+        return Criteria.<Coach>of(entityManager)
                 .root(Coach.class)
-                .where((builder, root) -> builder.notEqual(root.get("traineeId"), traineeId))
                 .where((builder, root) -> builder.isTrue(root.get("isActive")))
+                .where((builder, root) -> builder.isNotMember(traineeRef, root.get("trainees")))
                 .list();
     }
 
