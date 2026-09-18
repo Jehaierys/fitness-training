970bf9be5020951a53355f879524ff6db0d7fc1d
Jehaierys <kawaysempai@gmail.com>
2026-09-18 12:29:09 +0500

    test: add scenario disabling

--- src/test/java/com/fitnesstraining/intergation/IntegrationTest.java
+++ src/test/java/com/fitnesstraining/intergation/IntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
@@ -14,6 +15,10 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @ConfigurationParameter(
         key = GLUE_PROPERTY_NAME,
         value = "com.fitnesstraining.intergation"
+)
+@ConfigurationParameter(
+        key = FILTER_TAGS_PROPERTY_NAME,
+        value = "not @disabled"
 )
 public class IntegrationTest {
 }
