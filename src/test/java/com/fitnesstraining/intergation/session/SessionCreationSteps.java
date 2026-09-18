c77ec6e72cf1d40c687601cb380b1313517e7467
Jehaierys <kawaysempai@gmail.com>
2026-09-18 12:32:04 +0500

    refactor

--- src/test/java/com/fitnesstraining/intergation/session/SessionCreationSteps.java
+++ src/test/java/com/fitnesstraining/intergation/session/SessionCreationSteps.java
@@ -1,6 +1,5 @@
 package com.fitnesstraining.intergation.session;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fitnesstraining.config.test.JwtTestHelper;
 import com.fitnesstraining.domain.dto.request.session.SessionRegistrationRequest;
@@ -15,28 +14,22 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import jakarta.persistence.EntityManager;
 import jakarta.servlet.http.Cookie;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Properties;
-import java.util.UUID;
 
-import static com.fitnesstraining.config.test.KafkaTestContainer.KAFKA;
 import static java.time.temporal.ChronoUnit.MINUTES;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-
+@Slf4j
 public class SessionCreationSteps {
 
     @Autowired
@@ -66,6 +59,7 @@ public class SessionCreationSteps {
     private CoachWorkload receivedCoachWorkload;
     private Session persistedSession;
 
+
     @Before
     @Transactional
     public void clearDatabase() {
@@ -74,6 +68,7 @@ public class SessionCreationSteps {
         entityManager.createQuery("DELETE FROM Coach").executeUpdate();
         entityManager.clear();
     }
+
 
     @Transactional
     @Given("a trainee and a coach prepared in the database")
@@ -96,6 +91,7 @@ public class SessionCreationSteps {
 
         traineeUsername = trainee.getUsername();
         coachUsername = coach.getUsername();
+
     }
 
     @Given("a valid session creation request")
@@ -112,18 +108,27 @@ public class SessionCreationSteps {
 
     @When("I send a POST request to {string} with the session creation request")
     public void sendPostRequestToWithSessionCreationRequest(String url) throws Exception {
-        responseStatus = mockMvc.perform(post(url)
-                        .cookie(jwtCookie)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(sessionCreationRequest)))
-                .andReturn()
-                .getResponse()
-                .getStatus();
+        try {
+            responseStatus = mockMvc.perform(post(url)
+                            .cookie(jwtCookie)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(sessionCreationRequest)))
+                    .andReturn()
+                    .getResponse()
+                    .getStatus();
+        } catch (Exception e) {
+            exception = e;
+        }
     }
 
 
     @Then("the response status should be {int}")
     public void responseStatusShouldBe(int expectedStatus) {
+
+        if (exception != null) {
+            Assertions.fail(exception);
+        }
+
         assertEquals(expectedStatus, responseStatus);
     }
 
